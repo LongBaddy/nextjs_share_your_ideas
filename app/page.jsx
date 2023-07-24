ab310151047b7fb1e3d32f7cbed2654221c5caf7
LONG <xulongcool@gmail.com>
2023-07-24 22:57:38 +1000

    adjust home page font

--- app/page.jsx
+++ app/page.jsx
@@ -3,19 +3,20 @@ import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
+      <h1 className="head_text text-center green_gradient">
         Tell me
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">
-          What you think about this page
-        </span>
+
+
       </h1>
+      <span className="head_text orange_gradient text-center">
+        What you think about this page
+      </span>
 
       <p className="desc text-center">
         Just share your thoughts about this page
       </p>
 
-      <Feed/>
+      <Feed />
     </section>
   )
 }
