6662796ebcd78defee3e100fba684448629e4fcc
LONG <xulongcool@gmail.com>
2023-07-24 22:31:54 +1000

    Done

--- components/IdeaCard.jsx
+++ components/IdeaCard.jsx
@@ -13,7 +13,7 @@ const IdeaCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const [copied, setCopied] = useState("");
 
   const handleProfileClick = () => {
-    console.log(post);
+    
 
     if (post.creator._id === session?.user.id) return router.push("/profile");
 
